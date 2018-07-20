f2c325e6cfb665c8d753529c7c5bb4042ed71c29
Irving Armenta <irving@irvingarmenta.com>
2018-07-20 15:19:57 +0900

    scss modules set up and test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import style from './app.module.scss'
 
-const App = () => <div>Hello World!</div>
+// scss modules test
+console.log(style.mainSection);
+
+const App = () => {
+  return(
+    <div>
+      <section className={style.mainSection}>
+      <p>Hello darkness my old friend</p>
+    </section>
+  </div>
+  )
+}
 
 export default hot(module)(App)
