5d25c8cf2f6c2320b1b5fa268f4bc3a8103b8e45
smndiaye <serigne.ndiaye@monstar-lab.com>
2018-08-15 18:02:19 +0900

    refs: api key updated

--- src/app_sync.js
+++ src/app_sync.js
@@ -2,5 +2,5 @@ export default {
   "graphqlEndpoint": "https://jnd4wcqbojgghnqjmepfpkfrgq.appsync-api.ap-northeast-1.amazonaws.com/graphql",
   "region": "ap-northeast-1",
   "authenticationType": "API_KEY",
-  "apiKey": "da2-bj6gdt4eszew7cgufhap3cvvam"
+  "apiKey": "da2-23xvopkrxfdxdcrgzuxsqmrfce"
 }
